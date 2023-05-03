22c377e45dd79937fc8145e87a38d8d37117a18c
soniabergamini <sounia96@outlook.it>
2023-05-03 18:01:06 +0200

    feat(js): add team object array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 /****************** FUNCTIONS ******************/
+
 /****************** VARIABLES ******************/
+const team = [
+    { name: "Wayne Barnett",    role: "Founder & CEO",          img: "wayne-barnett-founder-ceo.jpg" },
+    { name: "Angela Caroll",    role: "Chief Editor",           img: "angela-caroll-chief-editor.jpg" },
+    { name: "Walter Gordon",    role: "Office Manager",         img: "walter-gordon-office-manager.jpg" },
+    { name: "Angela Lopez",     role: "Social Media Manager",   img: "angela-lopez-social-media-manager.jpg" },
+    { name: "Scott Estrada",    role: "Developer",              img: "scott-estrada-developer.jpg" },
+    { name: "Barbara Ramos",    role: "Graphic Designer",       img: "barbara-ramos-graphic-designer.jpg" },
+]
+
 /******************   INIT    ******************/
+
 /******************  EVENTS   ******************/
